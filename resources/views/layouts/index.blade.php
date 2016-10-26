58f5d727ac5fe6b9a475df350d038d7fdedd6bf7
kubili <kubili2013@gmail.com>
2016-10-26 13:57:30 +0800

    网站角标favicon,网站关键字,网站描述

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,6 +6,7 @@
         $dists = \App\Dictionary::where('parent_id', $type->id)->get();
         ?>
         <meta charset="utf-8">
+        <link rel="shortcut icon" href="/favicon.ico" />
         <meta content="">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
